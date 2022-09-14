459c36ced563b3b2380a8dc99e5dc73ee41408ee
JULIOMENA986 <20170157@uttcampus.edu.mx>
2022-09-14 10:06:44 -0500

    proyecto cotizador_vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+      <Header />
+          <div class="container">
+              
+
+              <form action="" class="form-horizontal">
+                  <div class="form-group left">
+                     <label for="" class="control-label col-sm-2">name</label>
+                     <div class="col-sm-10">
+                        <input type="text" class="form-control" name="nombre" id="nombre" v-model="form.name">
+                     </div>
+                  </div>
+                  <div class="form-group left">
+                     <label for="" class="control-label col-sm-2">email</label>
+                     <div class="col-sm-10">
+                        <input type="text" class="form-control" name="direccion" id="direccion" v-model="form.email">
+                     </div>
+                  </div>
+                  <div class="form-group left row">
+                    <div class="col">
+                          <label for="" class="control-label col-sm-3">password</label>
+                          <div class="col-sm-7">
+                              <input type="text" class="form-control" name="correo" id="correo" v-model="form.password">
+                          </div>
+                      </div>
+                      <div class="col">
+                        <label for="" class="control-label col-sm-5">password_confirmation</label>
+                        <div class="col-sm-7">
+                            <input type="text" class="form-control" name="codigopostal" id="codigopostal" v-model="form.password_confirmation">
+                        </div>
+                      </div> 
+                  </div>
+               
+
+
+                  <div class="form-group">
+                    <button type="button" class="btn btn-primary" v-on:click="guardar()" >Guardar</button>
+                    <button type="button" class="btn btn-dark margen" v-on:click="salir()"  >Salir</button>
+                  </div> 
+              </form>
+
+
+          </div>
+      <!-- <Footer /> -->
+
   </div>
 </template>
+<script>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
+//import Footer from '@/components/Footer.vue'
+import axios from 'axios';
+export default {
+  name:"HomeView",
+  data:function(){
+      return {
+          form:{
+              "name" : "",
+              "email": "", 
+              "password" : "",
+              "password_confirmation":"",
+              "token" : "" 
+          }
+      }
   },
-})
-export default class HomeView extends Vue {}
+ 
+  methods:{
+      guardar(){
+          this.form.token = localStorage.getItem("token");
+          axios.post("http://10.232.169.141:80/api/auth/register",this.form)
+          .then(data =>{
+              console.log(data);
+              this.makeToast("Hecho","pablo creado");
+              this.$router.push("/dashboard");
+          }).catch( e =>{
+              console.log(e);
+               this.makeToast("Error","Error al guardar","error");
+          })
+      },
+      salir(){
+          this.$router.push("/dashboard");
+      },
+      makeToast(titulo,texto,tipo) {
+          this.toastCount++
+          this.$bvToast.toast(texto, {
+          title: titulo,
+          variant: tipo,
+          autoHideDelay: 5000,
+          appendToast: true
+          })
+      }
+      
+  }
+}
 </script>
+<style scoped>
+.left{
+  text-align:  left;
+}
+</style>
